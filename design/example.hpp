1f92c8ad0b19ec392a55b9787efe22881c0cf0c7
srussame <sutawith001@gmail.com>
2025-12-27 21:51:28 +0700

    .

--- design/example.hpp
+++ design/example.hpp
@@ -56,6 +56,7 @@
 //----------- LIMITS
 // Soft limit of file descriptor allowed in this project
 # define MAX_FD 1024
+# define MAX_LISTENSOCKET_CONNECTION 100
 
 // Color Text
 # define  	LC_RED  				"\033[31m"
@@ -173,22 +174,174 @@ std::string Logger::getTimestamp()
 	strftime(buff, sizeof(buff) , "[%Y-%m-%d %H:%M:%S] " , localtime(&t));
 	return std::string(buff);
 }
+/*||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||*/
+/*||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||*/
+
+// Usage: like int fd = somegetfdfunction()
+// The main difference is you should not assign fd that always need to open
+// like 0, 1, 2
+// FileDescriptor a = open(file)   and toss this class around is fine.
+class FileDescriptor {
+	private:
+		int		_fd;
+		size_t*	_reference_counter;
+
+		void	release(){
+			if (_reference_counter){
+				(*_reference_counter)--;
+				if (*_reference_counter == 0){
+					if (close(_fd) != 0)
+						std::cerr << "FileDescriptor::close() failed" << std::strerror(errno) << std::endl;
+					delete _reference_counter;
+					_reference_counter = NULL;
+				}
+			}
+		}
+	public:
+		FileDescriptor() : _fd(-1), _reference_counter(NULL){
+		}
+		FileDescriptor(int fd) : _fd(fd), _reference_counter(NULL){
+			if (_fd < 0 || _fd >= MAX_FD){
+				std::string errMsg = "FileDescriptor::fd out of range::";
+				throw WebservException(errMsg);
+			}
+			try {
+				_reference_counter = new size_t(1);
+			} catch (std::exception &e){
+				if (close(_fd) != 0)
+					std::cerr << "FileDescriptor::close() failed" << std::strerror(errno) << std::endl;
+				throw;
+			}
+		};
+		FileDescriptor(const FileDescriptor& obj) : _fd(obj._fd), _reference_counter(obj._reference_counter){
+			if (_reference_counter)
+				(*_reference_counter)++;
+		}
+		FileDescriptor&	operator=(const FileDescriptor &obj){
+			if (this != &obj){
+				release();
+				_reference_counter = obj._reference_counter;
+				if (_reference_counter)
+					(*_reference_counter)++;
+				_fd = obj._fd;
+			}
+			return (*this);
+		}
+		FileDescriptor& operator=(int fd){
+			if (fd < 0 || fd >= MAX_FD){
+				std::string errMsg = "FileDescriptor::fd out of range::";
+				throw WebservException(errMsg);
+			}
+			if (_fd != fd){
+				size_t	*new_reference_counter = NULL;
+				try {
+					new_reference_counter = new size_t(1);
+				} catch (std::exception &e){
+					if (close(fd) != 0)
+						std::cerr << "FileDescriptor::close() failed" << std::strerror(errno) << std::endl;
+					throw;
+				}
+				release();
+				_fd = fd;
+				_reference_counter = new_reference_counter;
+			}
+			return (*this);
+		}
+		~FileDescriptor(){
+			release();
+		}
+
+		operator int() const {
+			return _fd;
+		};
+};
 
 /*||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||*/
 /*||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||*/
 
-typedef std::map<std::string, std::string>	t_config_map;
-typedef std::map<std::string, t_config_map>	t_location_map;
+
+/*||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||*/
+/*||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||*/
+
+typedef std::map<std::string, const std::string>	t_config_map;
+typedef std::map<std::string, const t_config_map>	t_location_map;
 
 class ServerConfig {
 	private:
 		t_config_map	_serverConfig;
-		t_location_map	_locationConfig;
+		t_location_map	_locationsConfig;
+
+		static void resolveLocationPath(std::string locationPath) {
+		}
+		const t_config_map* longestPrefixMatch(std::string locationPath) const {
+		    while (true) {
+			
+		        // 2. Use map::find (Fast lookup)
+		        t_location_map::const_iterator it = _locationsConfig.find(locationPath);
+			
+		        // 3. If found, this is inevitably the "longest" match 
+		        // because we started with the longest possible string.
+		        if (it != _locationsConfig.end()) {
+		            return &(it->second);
+		        }
+			
+		        // 4. If locationPath is empty or just root, and we still haven't found it, stop.
+		        if (locationPath.empty() || locationPath == "/") {
+		            return (NULL);
+		        }
+			
+		        // 5. Trim logic: Remove everything after the last slash
+		        size_t lastSlash = locationPath.find_last_of('/');
+			
+		        if (lastSlash == std::string::npos) {
+		            locationPath = ""; // No slashes left? Clear it.
+		        } else if (lastSlash == 0) {
+		            locationPath = "/"; // Only slash was at start? Make it root.
+		        } else {
+		            locationPath = locationPath.substr(0, lastSlash); // Cut off the last segment
+		        }
+		    }
+		}
+	public:
+		ServerConfig(){}
+		ServerConfig(const ServerConfig& obj): _serverConfig(obj._serverConfig), _locationsConfig(obj._locationsConfig){
+		}
+		ServerConfig& operator=(const ServerConfig& obj){
+			if (this != &obj){
+				_serverConfig = obj._serverConfig;
+				_locationsConfig = obj._locationsConfig;
+			}
+			return (*this);
+		}
+		~ServerConfig();
+
+		std::string getServerData(const std::string& keyToFind) const {
+			t_config_map::const_iterator FoundKey = _serverConfig.find(keyToFind);
+			if (FoundKey == _serverConfig.end())
+				return ("");
+			return (FoundKey->second);
+		}
+		std::string	getLocationData(const std::string& locationPath, const std::string& keytoFind) const {
+			const t_config_map* locationConfig = longestPrefixMatch(locationPath);
+			t_config_map::const_iterator foundKey;
+			if (locationConfig){
+				foundKey = locationConfig->find(keytoFind);
+					if (foundKey != locationConfig->end()){
+						return (foundKey->second);
+					}
+			}
+			foundKey = _serverConfig.find(keytoFind);
+			if (foundKey != _serverConfig.end()){
+				return (foundKey->second);
+			}
+			return ("");
+		}
 };
 
 class ConfigData {
 	private:
-		std::map<std::string, ServerConfig>	_serversConfigs;
+		// search server config by its port
+		std::map<int, std::vector<ServerConfig>>	_serversConfigs;
 
 		std::vector<std::string>	splitToken(std::string& readBuffer)
 		{
@@ -246,17 +399,148 @@ class ConfigData {
 /*||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||*/
 /*||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||*/
 
+class HttpRequest {
+
+};
+
+/*||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||*/
+/*||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||*/
+
+class HttpResponse {
+
+};
+
+
+/*||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||*/
+/*||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||*/
+
+class	ClientConnection {
+
+};
+
+/*||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||*/
+/*||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||*/
+
+enum e_socket_type {
+	NO_TYPE,
+	CLIENT_SOCKET,
+	SERVER_SOCKET,
+	CGI_SOCKET
+};
+
+class Socket{
+	private:
+
+		FileDescriptor	_socketFD;
+		e_socket_type	_socketType;
+		int				_server_listen_port;
+
+	public:
+		const std::map<std::string, const ServerConfig>	*serversConfig;
+
+		Socket() : _socketType(NO_TYPE), serversConfig(NULL), _server_listen_port(-1){};
+		Socket(const Socket& obj) :  _socketFD(obj._socketFD), _socketType(obj._socketType),
+		serversConfig(obj.serversConfig), _server_listen_port(obj._server_listen_port){
+
+		}
+		Socket(int fd) : _socketFD(fd), _socketType(NO_TYPE), serversConfig(NULL) {
+		}
+		Socket&	operator=(const Socket& obj){
+			if (this != &obj){
+				_socketFD = obj._socketFD;
+				_socketType = obj._socketType;
+				serversConfig = obj.serversConfig;
+			}
+			return (*this);
+		}
+		~Socket(){
+
+		}
+
+		void setupSocket(e_socket_type socketType){
+			if (_socketType != NO_TYPE){
+				Logger::log(LC_RED, "ERROR::Socket::setSocketType::SET NEW TYPE TO ALREADY SET ONE");
+				return ;
+			}
+			switch (socketType) {
+				case SERVER_SOCKET:
+					if (!serversConfig){
+						throw WebservException("Socket::setup ServerSocket needs serversConfig!");
+					}
+					if (_socketFD < 0){
+						int temp_fd = socket(AF_INET, SOCK_STREAM, 0);
+						if (temp_fd < 0){
+							std::string errorMsg = "::Socket::setSocketType::socket() failed::";
+							errorMsg += std::strerror(errno);
+							throw WebservException(errorMsg);
+						}
+						_socketFD = temp_fd;
+					}
+
+					if (fcntl(_socketFD, F_SETFL, O_NONBLOCK) != 0)
+					{
+						std::string errorMsg = "Socket::fcntl() O_NONBLOCK Error::";
+						errorMsg += std::strerror(errno);
+						throw WebservException(errorMsg);
+					}
+					
+					// reusable socket if the server was restart before port allocation timeout
+					int opt = 1;
+					if(setsockopt(_socketFD, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0)
+						Logger::log(LC_NOTE, "Fail to set socket#%d for reuse socket", _socketFD);
+					
+					sockaddr_in sv_addr;
+					std::memset(&sv_addr, 0, sizeof(sockaddr));
+					sv_addr.sin_family = AF_INET;
+					sv_addr.sin_addr.s_addr = INADDR_ANY;
+					/*################### NEED TO FIX THIS */
+					_server_listen_port = 2345;
+					sv_addr.sin_port = htons(_server_listen_port);
+
+					if (bind(_socketFD, (struct sockaddr*)&sv_addr, sizeof(sv_addr)) != 0){
+						std::string errorMsg = "Socket::bind() failed::";
+						errorMsg += std::strerror(errno);
+						throw WebservException(errorMsg);
+					}
+
+					if (listen(_socketFD, MAX_LISTENSOCKET_CONNECTION) != 0){
+						std::string errorMsg = "Socket::listen() failed::";
+						errorMsg += std::strerror(errno);
+						throw WebservException(errorMsg);
+					}
+					std::map<std::string, const ServerConfig>::const_iterator it = serversConfig->begin();
+
+					std::stringstream ss;
+					ss << _server_listen_port;
+					std::string portStr;
+					ss >> portStr;
+					while (it != serversConfig->end()){
+						Logger::log(LC_SYSTEM, "Server <" + it->first + "> is listening on port " + portStr);
+						++it;
+					}
+					break;
+			}
+		}
+};
+
+/*||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||*/
+/*||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||*/
+
 class WebServ {
 	private:
 		// the .conf file that we passed to the program
 		const std::string	_webservConfigPath;
 		ConfigData			_configData;
+		FileDescriptor		_epollFD;
 
 	public:
 		WebServ(const std::string& configPath) : _webservConfigPath(configPath), _configData(_webservConfigPath)
 		{
 			// Handle ConfigPath here or
 		}
+
+		void	run();
+
 };
 
 /*||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||*/
